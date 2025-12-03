b192b10fe4886170bd0b2be40acadd1d5488d458
AndresC <d.rodman213@gmail.com>
2025-12-03 14:45:57 -0500

    chore: Update coverage directory to point to frontend/docs/test/backend

--- jest.config.js
+++ jest.config.js
@@ -1,34 +1,24 @@
-module.exports = {
-    testEnvironment: 'node',
-    coverageDirectory: 'coverage',
-    collectCoverageFrom: [
-        'src/services/**/*.js',
-        'src/repositories/**/*.js',
-        '!src/**/*.test.js',
-        '!src/**/index.js'
-    ],
-    coverageThreshold: {
-        global: {
-            statements: 70,
-            branches: 70,
+global: {
+    statements: 70,
+        branches: 70,
             functions: 70,
-            lines: 70
-        },
-        './src/services/': {
-            statements: 70,
-            branches: 70,
+                lines: 70
+},
+'./src/services/': {
+    statements: 70,
+        branches: 70,
             functions: 70,
-            lines: 70
-        },
-        './src/repositories/': {
-            statements: 70,
-            branches: 70,
+                lines: 70
+},
+'./src/repositories/': {
+    statements: 70,
+        branches: 70,
             functions: 70,
-            lines: 70
-        }
+                lines: 70
+}
     },
-    coverageReporters: ['text', 'lcov', 'html'],
+coverageReporters: ['text', 'lcov', 'html'],
     testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'],
-    verbose: true,
-    testTimeout: 10000
+        verbose: true,
+            testTimeout: 10000
 };
